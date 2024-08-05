bb78add3705fab6498ae9c57e4afe9c6c7d94c27
Williams Hernández <hernandw@gmail.com>
2024-08-05 18:27:34 -0400

    :estructura de clase 2

---
+++ src/data/data.js
@@ -0,0 +1,53 @@
+export const shoes = [
+  {
+    id: 1,
+    name: "Girls's Court High - 	Hi Ridge",
+    images: '/images/shoes1.png',
+    price: 34900,
+  },
+  {
+    id: 2,
+    name: "Girl's Court High - Shine Kicks",
+    images: '/images/shoes2.png',
+    price: 34900,
+  },
+  {
+    id: 3,
+    name: "Girls'Hi Ridge",
+    images: '/images/shoes3.png',
+    price: 33990,
+  },
+  {
+    id: 4,
+    name: "Girls' Hi Ridge - Superstardom",
+    images: '/images/shoes4.png',
+    price: 36990,
+  },
+  {
+    id: 5,
+    name: "Girls' Eden LX",
+    images: '/images/shoes5.png',
+    price: 32990,
+  },
+
+  {
+    id: 6,
+    name: "Girls' Hi Ridge",
+    images: '/images/shoes6.png',
+    price: 30990,
+  },
+
+  {
+    id: 7,
+    name: "Girls' Hi Ridge",
+    images: '/images/shoes7.png',
+    price: 30990,
+  },
+
+  {
+    id: 8,
+    name: "Girls' Hi Ridge",
+    images: '/images/shoes8.png',
+    price: 30990,
+  },
+];
